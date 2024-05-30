6b411317887e24c6159816eb461025f72c525909
VitoMarino <marino.vito2001@gmail.com>
2024-05-30 12:57:14 +0200

    Inizio esercizio

---
+++ script.js
@@ -0,0 +1,23 @@
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+            toDoList : [{
+                text: 'Patate',
+                done: 'false'
+            }, 
+            {
+                text: 'Carote',
+                done: 'true',
+            },
+            {
+                text: 'Formaggio',
+                done: 'false'
+            }]
+        }
+    },
+    methods : {
+        
+    }
+}).mount('#app')
