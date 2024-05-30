bd9c23adce4c45c5f98fe491a3d1dd708d9dfe87
VitoMarino <marino.vito2001@gmail.com>
2024-05-30 18:53:45 +0200

    add task

--- script.js
+++ script.js
@@ -14,13 +14,28 @@ createApp({
             {
                 text: 'Formaggio',
                 done: false
-            }]
+            }],
+            newTaskContent: '',
         }
     },
     // Con "oggettoIndex" ho fatto in modo che entrasse dentro i vari oggetti  dell'array
     methods: {
         onOffClick: function(oggettoIndex) {
             this.toDoList[oggettoIndex].done = !this.toDoList[oggettoIndex].done
+        },
+        addTask: function (passaggio) {
+                const newTask = {
+                    text: passaggio.trim(),
+                    done: false,
+                }
+                this.toDoList.push(newTask);
+                this.clearNewTaskInput();
+            },
+        clearNewTaskInput: function() {
+            this.newTaskContent = '';
+        },
+        removeTask: function(oggettoIndex) {
+            this.toDoList.splice(oggettoIndex, 1)
         }
-    }
-}).mount('#app')
+    },
+}).mount('#app');
